08fbbd17998211c4aa5dab44ec61eac8def03e22
Andrea <andrea.erikson@gmail.com>
2018-11-17 12:14:05 -0700

    Added firebase to html and js

---
+++ script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDE2vhxcpy_mIQ7xlURKPlY_d4YnLyR0PE",
+    authDomain: "musicproject-77064.firebaseapp.com",
+    databaseURL: "https://musicproject-77064.firebaseio.com",
+    projectId: "musicproject-77064",
+    storageBucket: "",
+    messagingSenderId: "858372923149"
+  };
+  firebase.initializeApp(config);

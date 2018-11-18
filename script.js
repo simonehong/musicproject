dfc6ce55e0e439bb69f41d05a856c7f4faa4db2f
David Sjoberg <davidito16@gmail.com>
2018-11-18 13:42:20 -0700

    I added links to our script file and jquery, started to input API

--- script.js
+++ script.js
@@ -10,6 +10,105 @@
   };
   firebase.initializeApp(config);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Create API link to Bandsintown
 // App ID = ab1539793d4956976bf4f8052a7ed8cb
 
+var artistName = "Taylor Swift"
+var queryBand = "https://rest.bandsintown.com/artists" +
+        artistName + "?app_id=ab1539793d4956976bf4f8052a7ed8cb";
+
+$.ajax({
+  url: queryBand,
+  method: "GET"
+}).then(function(response) {
+
+  // Printing the entire object to console
+  console.log(response);
+});
